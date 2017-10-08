44f76c6d40f83f1548668fa8ea1be5f3ce2deb68
Robert Dresler <dresler.robert@gmail.com>
2017-10-08 21:46:48 +0200

    Naming rules for test classes and methods

---
+++ MyCompany.MyProject/MaxPairValueCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCompany.MyProject
+{
+    public class MaxPairValueCalculator
+    {
+        public int Calculate(params int[] dice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests.MyCompany.MyProject/MaxPairValueCalculatorTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tests.MyCompany.MyProject
+{
+    [TestFixture]
+    public class MaxPairValueCalculatorTests
+    {
+        [Test]
+        public void Calculate_ForEmptyDice_ShouldReturnZero()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Calculate_ForDiceWithoutAnyPair_ShouldReturnZero()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Calculate_ForDiceWithJustOnePair_ShouldReturnSumOfThePair()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Calculate_ForDiceWithTwoPairs_ShouldReturnSumOfTheHigherPair()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Wrong names
+
+        // Cryptic non-explanatory name
+        [Test]
+        public void TestA()
+        {
+        }
+
+        // Just a piece of information (UnitOfWork), StateUnderTest and ExpectedBehavior missing
+        [Test]
+        public void TestCalculate1()
+        {
+        }
+
+        // Just StateUnderTest
+        [Test]
+        public void TestNoPair()
+        {
+        }
+
+        // Missing UnitOfWork
+        [Test]
+        public void ItShouldReturnZeroForEmptyDices()
+        {
+        }
+
+        // Confusing order of ExpectedBehavior, UnitOfWork, StateUnderTest
+        [Test]
+        public void ReturnZeroWhenCalculatesDiceWithoutPair()
+        {
+        }
+
+        // It describes a particular test case instead of the equivalence partition
+        [Test]
+        public void Calculate_For123345_ShouldReturn6()
+        {
+        }
+    }
+}
